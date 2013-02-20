f2638c608715f52c9814c760bd3cdff6e76e9522
Daniel Schuba <dasch@daschmedia.de>
2013-02-20 19:58:01 +0100

    improved translation

--- JavascriptSlideshow.i18n.php
+++ JavascriptSlideshow.i18n.php
@@ -16,7 +16,7 @@ $messages['en'] = array(
 	'javascriptslideshow-desc'  => 'Create a slideshow from multiple included div elements',
 	'javascriptslideshow-error-html5' => '$wgHtml5 must be set to true for this extension to work',
 	'javascriptslideshow-error-unkown' => 'Something went totaly wrong when we ended up here',
-	'javascriptslideshow-invalid-parameter' => 'Invalid parameter $1 $2 (May be one of: $3). ',
+	'javascriptslideshow-invalid-parameter' => 'Invalid value $2 for parameter $1 (May be one of: $3). ',
 	'javascriptslideshow-invalid-num-parameter' => 'Parameter $1 must be a number',
 );
 
@@ -24,7 +24,7 @@ $messages['de'] = array(
 	'javascriptslideshow-desc'  => 'Erzeugt eine Slideshow aus in div eingebetteten Elementen',
 	'javascriptslideshow-error-html5' => '$wgHtml5 muss true sein damit diese Erweiterung funktioniert',
 	'javascriptslideshow-error-unkown' => 'Irgendwas ist wirklich komplett schiefgelaufen wenn wir hier angekommen sind',
-	'javascriptslideshow-invalid-parameter' => 'Nicht unterstützer Parameter $1 $2 (Erlaubt sind: $3). ',
+	'javascriptslideshow-invalid-parameter' => 'Nicht unterstützer Wert $2 für Parameter $1 (Erlaubt sind: $3). ',
 	'javascriptslideshow-invalid-num-parameter' => 'Der Parameter $1 muss eine Zahl sein',
 );
 ?>
